9282e54799aa8cb07e92502631a187671a1e0842
Yefferson Solis <yefferson@gisoft.co>
2021-11-18 10:52:25 -0500

    feat: modificación de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Entities;
 use App\Models\Nodes;
 use App\Models\TypeNodes;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,12 +25,39 @@ class DatabaseSeeder extends Seeder
             'name' => 'Juego'
         ]);
 
-        Nodes::factory()->create();
+        /* Nodes::factory()->create(); */
+        Nodes::create([
+            'type_node_id' => '1'
+        ]);
 
-        Entities::factory()->create();
+        /* Entities::factory()->create(); */
+        Entities::create([
+            'node_id' => 1,
+            'zona_horaria' => 'GMT-5',
+            'moneda' => 'COP',
+            'nombre_contacto' => 'Principal',
+            'telefono_contacto' => '3111111111',
+            'email' => 'email@email.com',
+            'pais' => 'COL',
+            'zona' => 'Meta',
+            'nit' => '911111111',
+            'balance' => '500000'
+        ]);
 
-        User::factory([
+        /* User::factory([
             'email' => 'email@email.com'
-        ])->create();
+        ])->create(); */
+        User::create([
+            'nombres' => 'Demarcus',
+            'apellidos' => 'Mills',
+            'documento' => 'corporis',
+            'telefono' => '1-234-850-6841',
+            'email' => 'email@email.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'node_id' => 1,
+            'tipo_usuario' => 'vendedor'
+        ]);
     }
 }
