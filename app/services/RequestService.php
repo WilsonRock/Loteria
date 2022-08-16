760083ade16b6157905413556928374f8505f433
Norbey <wilson.norbey@gisoft.co>
2022-08-16 10:03:42 -0500

    feat:Campo provider en tabla games, se cambia Apkeys y tokens

--- app/services/RequestService.php
+++ app/services/RequestService.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Http;
 use PhpParser\Node\Stmt\TryCatch;
 
 class RequestService {
-  use ConsumeExternalServices;
+ // use ConsumeExternalServices;
 
   protected $providerRepository;
   protected $baseUri;
@@ -46,11 +46,11 @@ class RequestService {
            ];
         $response = Http::withHeaders([
             "Accept"=> "*/*",
-            'AppKey' => 'betappkey-2-B0A1C1',
-            'AppToken' => '59SGQ81SHPUB1XLJC1FOR0OSYCE1G7ICZTM1XI',
+            'AppKey' => 'betappkey-2-DBB100',
+            'AppToken' => 'JC05HQPDAEQLMJS003FQX6AZGIQDQT7N2BOHX',
             'Content-Type' => 'application/json'
       
-        ])->get('http://localhost:3000/games/'.$data['codigoprovider'],
+        ])->get('https://bet-api-provider.herokuapp.com/games/'.$data['codigoprovider'],
     
         );
 
@@ -79,27 +79,37 @@ class RequestService {
   public function SalesQuery(Request $data)
 
   {
-    $req = $data->vendidos;
-   // dd($data->cliente_id);
-    //echo($data);
-    $data = [
-          'bet_number' => $data['bet_number'],
-          'amount' => $data['valor'],
-          'commerce'=> 2,
-          'raffle'=> 1,
-          'stateSale'=> 1
-      ];
-     // $response = $this->makeRequest('POST', $this->baseUri . "superpagos/products/query", [], $data);
-    //$response = $this->makeRequest("POST", "http://localhost:3000" . "/sales", [], $data ,  Http::withHeaders(['headers' => ['ApiKey' => 'betappkey-2-B0A1C1','ApiToken' => '59SGQ81SHPUB1XLJC1FOR0OSYCE1G7ICZTM1XI'] ]) );
-    $response = Http::withHeaders([
-        'AppKey' => 'betappkey-2-B0A1C1',
-        'AppToken' => '59SGQ81SHPUB1XLJC1FOR0OSYCE1G7ICZTM1XI'
+    try {
+
+        //code...
+        $req = $data->vendidos;
+         //dd($data->cliente_id);
+         echo($data);
+         $data = [
+               'bet_number' => $data['bet_number'],
+               'amount' => $data['valor'],
+               'commerce'=> 1,
+               'raffle'=> 1,
+               'stateSale'=> 1
+           ];
+          // $response = $this->makeRequest('POST', $this->baseUri . "superpagos/products/query", [], $data);
+         //$response = $this->makeRequest("POST", "http://localhost:3000" . "/sales", [], $data ,  Http::withHeaders(['headers' => ['ApiKey' => 'betappkey-2-B0A1C1','ApiToken' => '59SGQ81SHPUB1XLJC1FOR0OSYCE1G7ICZTM1XI'] ]) );
+         $response = Http::withHeaders([
+            "Accept"=> "*/*",
+            'AppKey' => 'betappkey-2-DBB100',
+            'AppToken' => 'JC05HQPDAEQLMJS003FQX6AZGIQDQT7N2BOHX',
+            'Content-Type' => 'application/json'
+             
+         ])->post('https://bet-api-provider.herokuapp.com/sales', 
+             $data
+         );
         
-    ])->post('http://localhost:3000/sales', 
-        $data
-    );
-    //echo($response);
- return response()->json(['message' => 'Juego creado con éxito','data' => $response], 201);
+      return response()->json(['message' => 'Juego creado con éxito','data' => $response], 201);
+    } catch (\Throwable $th) {
+        echo($response);
+        //throw $th;
+    }
+   
   }
 
   public function findProductsByProvider($provider)
